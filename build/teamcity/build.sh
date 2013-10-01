e7a7f30467deb527589dc8feaa4bee5695df6024
Matthew O'Brien <matthew.obrien@theguardian.com>
2013-10-01 11:17:40 +0100

    Riffraff

--- build/teamcity/build.sh
+++ build/teamcity/build.sh
@@ -7,7 +7,7 @@ fi
 
 mkdir -p dist/artifacts
 cp deploy.json dist/artifacts
-mkdir -p dist/artifacts/packages/robots.txt
+mkdir -p dist/artifacts/packages/robotstxt
 zip --junk-paths dist/artifacts/packages/robotstxt/robots.txt.zip robots.txt
 cd dist/artifacts && zip ../artifacts.zip deploy.json packages/robotstxt/robots.txt.zip
 
